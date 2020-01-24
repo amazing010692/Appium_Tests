6213e5cc8d269da3a40bdab51f0a0b0c31633b17
Janielle Gregorio <hello_janielle1992@yahoo.com.ph>
2020-01-24 16:13:11 +0800

    Added options to run in an emulator or in a real device.

--- Appium_Tests/src/base.java
+++ Appium_Tests/src/base.java
@@ -2,7 +2,6 @@ import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.remote.DesiredCapabilities;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
@@ -16,7 +15,11 @@ public class base {
 		File fs = new File(f, "ApiDemos-debug.apk");
 				
 		DesiredCapabilities cap = new DesiredCapabilities();
-		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "JanielleEmulator");
+		
+		//	Just comment out the 2 lines below if you want to run in an emulator or in a real device.
+		// cap.setCapability(MobileCapabilityType.DEVICE_NAME, "JanielleEmulator");
+		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Android Device");
+		
 		cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "uiautomator2");
 		cap.setCapability(MobileCapabilityType.APP, fs.getAbsolutePath());
 		AndroidDriver<AndroidElement> driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), cap);
