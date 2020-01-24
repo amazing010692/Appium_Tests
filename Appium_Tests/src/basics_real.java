d1b87486fbcd16000b6bdddfc0a1c30ee8cbc368
Janielle Gregorio <hello_janielle1992@yahoo.com.ph>
2020-01-24 17:25:32 +0800

    Added hybrid_base for emulator and real device.

---
+++ Appium_Tests/src/basics_real.java
@@ -0,0 +1,29 @@
+import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+
+public class basics_real extends hybrid_base {
+
+	public static void main(String[] args) throws MalformedURLException {
+		
+		// This code will run in a real device with remote USB debugging.
+		AndroidDriver<AndroidElement> driver = Capabilities("real");
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		
+		//xpath id, className, androidUIautomator
+		/*		xpath Syntax
+		 * 		//tagName[@attribute='value']
+		 */
+		driver.findElementByXPath("//android.widget.TextView[@text='Preference']").click();
+		driver.findElementByXPath("//android.widget.TextView[@text='3. Preference dependencies']").click();
+		driver.findElementById("android:id/checkbox").click();
+		driver.findElementByXPath("(//android.widget.RelativeLayout)[2]").click();
+		driver.findElementByClassName("android.widget.EditText").sendKeys("hello Jan");
+		driver.findElementsByClassName("android.widget.Button").get(1).click();
+		
+	}
+
+}
