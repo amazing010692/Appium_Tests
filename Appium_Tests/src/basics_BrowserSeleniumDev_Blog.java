148df7ec6822a7f184241346499f9344b88776b2
Janielle Gregorio <hello_janielle1992@yahoo.com.ph>
2020-02-03 12:54:46 +0800

    Selenium Blog

---
+++ Appium_Tests/src/basics_BrowserSeleniumDev_Blog.java
@@ -0,0 +1,26 @@
+import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+
+public class basics_BrowserSeleniumDev_Blog extends base_Chrome {
+
+	public static void main(String[] args) throws MalformedURLException, InterruptedException {
+		// TODO Auto-generated method stub
+		
+		AndroidDriver<AndroidElement> driver = Capabilities();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		driver.get("https://selenium.dev/");
+		System.out.println(driver.getTitle());
+		//	Click the Menu button.
+		driver.findElementByXPath("//*[@id=\"dropdownButton\"]").click();
+		//	Click the Documentation option.
+		driver.findElementByLinkText("Documentation").click();
+		System.out.println(driver.getCurrentUrl());
+		Thread.sleep(3000);
+		driver.quit();
+		
+	}
+
+}
